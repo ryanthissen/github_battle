a739fe6b32862674835f9f50fe7f7059ab0a09a1
Ryan Thissen <rmt1855@gmail.com>
2017-11-14 12:21:38 -0800

    added PropType validations

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
+let PropTypes = require('prop-types');
+
+
 import Badge from './badge.js';
 require('./index.css');
 
@@ -13,13 +16,19 @@ class App extends React.Component {
   }
 }
 
+Badge.propTypes = {
+  name: PropTypes.string.isRequired,
+  img:PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+}
+
 ReactDOM.render(
   <div>
     <App />
     <Badge
-      name='Black Kettle'
-      username='blackkettle22'
-      img='https://i.pinimg.com/originals/04/1d/de/041ddee2d7d9ab458b1c04e0cf76e633.jpg'
+      name={'Black Kettle'}
+      username={'blackkettle22'}
+      img={'https://i.pinimg.com/originals/04/1d/de/041ddee2d7d9ab458b1c04e0cf76e633.jpg'}
     />
   </div>
   ,

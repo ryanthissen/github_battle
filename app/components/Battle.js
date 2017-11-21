523862b385ad34260ee116e669453e95cf262fa3
Ryan Thissen <rmt1855@gmail.com>
2017-11-21 12:19:48 -0800

    added battle components, styling

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,83 @@
 const React = require('react');
+let PropTypes = require('prop-types');
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className='column'>
+        <img className='avatar' src={props.avatar} alt={'Avatar for ' + props.username}/>
+      </div>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+
+    this.setState(function() {
+      return {
+        username: value,
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+
+  render() {
+    return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
 
 export default class Battle extends React.Component {
   constructor(props) {
